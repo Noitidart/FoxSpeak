e13cff625bc93874613444b57b01df98f88eed53
Abhilash Mhaisne <55abhilash@openmailbox.org>
2015-12-14 09:22:31 +0530

    Committed : pass_audio_file.c

---
+++ modules/pass_audio_file.c
@@ -0,0 +1,174 @@
+/* -*- c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/* ====================================================================
+ * Copyright (c) 1999-2010 Carnegie Mellon University.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * This work was supported in part by funding from the Defense Advanced 
+ * Research Projects Agency and the National Science Foundation of the 
+ * United States of America, and the CMU Sphinx Speech Consortium.
+ *
+ * THIS SOFTWARE IS PROVIDED BY CARNEGIE MELLON UNIVERSITY ``AS IS'' AND 
+ * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY
+ * NOR ITS EMPLOYEES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ====================================================================
+ *
+ */
+/*
+ * continuous.c - Simple pocketsphinx command-line application to test
+ *                both continuous listening/silence filtering from microphone
+ *                and continuous file transcription.
+ */
+
+/*
+ * This is a simple example of pocketsphinx application that uses continuous listening
+ * with silence filtering to automatically segment a continuous stream of audio input
+ * into utterances that are then decoded.
+ * 
+ * Remarks:
+ *   - Each utterance is ended when a silence segment of at least 1 sec is recognized.
+ *   - Single-threaded implementation for portability.
+ *   - Uses audio library; can be replaced with an equivalent custom library.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#if defined(_WIN32) && !defined(__CYGWIN__)
+#include <windows.h>
+#else
+#include <sys/select.h>
+#endif
+
+#include <sphinxbase/err.h>
+#include <sphinxbase/ad.h>
+
+#include "pocketsphinx.h"
+
+static ps_decoder_t *ps;
+static cmd_ln_t *config;
+static FILE *rawfd;
+
+
+static int
+check_wav_header(char *header, int expected_sr)
+{
+    int sr;
+
+    if (header[34] != 0x10) {
+        E_ERROR("Input audio file has [%d] bits per sample instead of 16\n", header[34]);
+        return 0;
+    }
+    if (header[20] != 0x1) {
+        E_ERROR("Input audio file has compression [%d] and not required PCM\n", header[20]);
+        return 0;
+    }
+    if (header[22] != 0x1) {
+        E_ERROR("Input audio file has [%d] channels, expected single channel mono\n", header[22]);
+        return 0;
+    }
+    sr = ((header[24] & 0xFF) | ((header[25] & 0xFF) << 8) | ((header[26] & 0xFF) << 16) | ((header[27] & 0xFF) << 24));
+    if (sr != expected_sr) {
+        E_ERROR("Input audio file has sample rate [%d], but decoder expects [%d]\n", sr, expected_sr);
+        return 0;
+    }
+    return 1;
+}
+
+/*
+ * Continuous recognition from a file
+ */
+static void
+recognize_from_file(char *filename, int srate)
+{
+    int16 adbuf[2048];
+    const char *fname;
+    const char *hyp;
+    int32 k;
+    uint8 utt_started, in_speech;
+
+    fname = filename;
+    if ((rawfd = fopen(fname, "rb")) == NULL) {
+        E_FATAL_SYSTEM("Failed to open file '%s' for reading",
+                       fname);
+    }
+    
+    if (strlen(fname) > 4 && strcmp(fname + strlen(fname) - 4, ".wav") == 0) {
+        char waveheader[44];
+	fread(waveheader, 1, 44, rawfd);
+	if (!check_wav_header(waveheader, srate))
+    	    E_FATAL("Failed to process file '%s' due to format mismatch.\n", fname);
+    }
+
+    if (strlen(fname) > 4 && strcmp(fname + strlen(fname) - 4, ".mp3") == 0) {
+	E_FATAL("Can not decode mp3 files, convert input file to WAV 16kHz 16-bit mono before decoding.\n");
+    }
+    
+    ps_start_utt(ps, NULL);
+    utt_started = FALSE;
+
+    while ((k = fread(adbuf, sizeof(int16), 2048, rawfd)) > 0) {
+        ps_process_raw(ps, adbuf, k, FALSE, FALSE);
+        in_speech = ps_get_in_speech(ps);
+        if (in_speech && !utt_started) {
+            utt_started = TRUE;
+        } 
+        if (!in_speech && utt_started) {
+            ps_end_utt(ps);
+            hyp = ps_get_hyp(ps, NULL, NULL);
+            if (hyp != NULL)
+        	printf("%s\n", hyp);
+
+            ps_start_utt(ps);
+            utt_started = FALSE;
+        }
+    }
+    ps_end_utt(ps);
+    if (utt_started) {
+        hyp = ps_get_hyp(ps, NULL, NULL);
+        if (hyp != NULL) {
+    	    printf("%s\n", hyp);
+	}
+    }
+    
+    fclose(rawfd);
+}
+
+/* Sleep for specified msec */
+static void
+sleep_msec(int32 ms)
+{
+#if (defined(_WIN32) && !defined(GNUWINCE)) || defined(_WIN32_WCE)
+    Sleep(ms);
+#else
+    /* ------------------- Unix ------------------ */
+    struct timeval tmo;
+
+    tmo.tv_sec = 0;
+    tmo.tv_usec = ms * 1000;
+
+    select(0, NULL, NULL, NULL, &tmo);
+#endif
+}
+
